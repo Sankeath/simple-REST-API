76a667951399b6923f30cc7e0df18f8e1f0e4cb0
Sankeath <s3897982@student.rmit.edu.au>
2022-10-11 04:10:13 +1100

    Added PersonController functionality

--- Backend/Person/src/main/java/com/example/person/Controller/PersonController.java
+++ Backend/Person/src/main/java/com/example/person/Controller/PersonController.java
@@ -3,16 +3,56 @@ package com.example.person.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.MethodNotAllowedException;
-import org.springframework.web.server.ResponseStatusException;
 
+import com.example.person.DAO.PersonDAO;
+import com.example.person.Model.Person;
 
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping(path = "/person")
+@RequestMapping(path = "/persons")
 public class PersonController {
 
+    @Autowired
+    private PersonDAO personDAO;
+
+    //Get all persons
+    @GetMapping(path = "/person", produces = "application/json")
+    public List<Person> getPersonList(){
+        return personDAO.getPersonList();
+    }
+
+    //Get person by id
+    @GetMapping(path = "/{id}", produces = "application/json")
+    public Person getPersonList(@PathVariable Long id){
+        Optional<Person> person = personDAO.getPerson(id);
+        return person.get();
+    }
+
+    //Update person using their id
+    @PutMapping(path = "/person", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<String> updatePerson(@RequestBody Person person){
+
+        if (personDAO.updatePerson(person.getId(),person)){
+            return new ResponseEntity<>("Person info updated",HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>("Person to update not found",HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping(path = "/person",consumes = "application/json",produces = "application/json")
+    public ResponseEntity<String> deletePerson(@RequestBody Person person){
+        personDAO.deletePerson(person.getId());
+        return new ResponseEntity<>("Person specified by id was deleted",HttpStatus.OK);
+    }
+
+    @PostMapping(consumes = "application/json",produces = "application/json")
+    public ResponseEntity<String> createPerson(@RequestBody Person person){
+        personDAO.createPerson(person);
+        return new ResponseEntity<>("Person created",HttpStatus.OK);
+    }
 
 }
