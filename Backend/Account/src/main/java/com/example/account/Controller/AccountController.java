e117c3177e151bd10a217a585dee096e0c113670
Sankeath <s3897982@student.rmit.edu.au>
2022-10-10 20:47:09 +1100

    made feature_branch for account controller

---
+++ Backend/Account/src/main/java/com/example/account/Controller/AccountController.java
@@ -0,0 +1,21 @@
+package com.example.account.Controller;
+
+//import com.example.onlineshop.DAO.OnlineShopDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.MethodNotAllowedException;
+import org.springframework.web.server.ResponseStatusException;
+
+
+
+@RestController
+@RequestMapping(path = "/account")
+public class AccountController {
+
+
+
+
+}
