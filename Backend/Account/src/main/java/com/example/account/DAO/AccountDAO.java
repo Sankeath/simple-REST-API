3dbe06768f83009370c9a1416fb8672abb679695
Sankeath <s3897982@student.rmit.edu.au>
2022-10-10 20:53:57 +1100

    made feature_branch for account DAO

---
+++ Backend/Account/src/main/java/com/example/account/DAO/AccountDAO.java
@@ -0,0 +1,14 @@
+package com.example.account.DAO;
+
+import org.springframework.stereotype.Repository;
+
+//import com.example.onlineshop.Model.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class AccountDAO {
+
+
+}
