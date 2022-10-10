4746893d4228c50ed8ca8054e9b413565ab42117
Sankeath <s3897982@student.rmit.edu.au>
2022-10-11 01:52:40 +1100

    added personDAO

--- Backend/Person/src/main/java/com/example/person/DAO/PersonDAO.java
+++ Backend/Person/src/main/java/com/example/person/DAO/PersonDAO.java
@@ -1,9 +1,9 @@
 package com.example.person.DAO;
 
+//import org.springframework.stereotype.Repository;
+//import org.springframework.stereotype.Repository;
+
 import org.springframework.stereotype.Repository;
-
-
-
 @Repository
 public class PersonDAO {
 
