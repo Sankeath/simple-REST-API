a01679d4ee288987c1e09c4ea28a1b9da7523c1f
Sankeath <s3897982@student.rmit.edu.au>
2022-10-10 21:04:14 +1100

    made feature_branch for person controller

---
+++ Backend/Person/src/main/java/com/example/person/Controller/PersonController.java
@@ -0,0 +1,18 @@
+package com.example.person.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.MethodNotAllowedException;
+import org.springframework.web.server.ResponseStatusException;
+
+
+
+@RestController
+@RequestMapping(path = "/person")
+public class PersonController {
+
+
+}
