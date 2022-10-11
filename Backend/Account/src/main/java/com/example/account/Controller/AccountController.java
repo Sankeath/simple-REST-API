d2b5667ad589118611f8002965bd14c17c7eef0a
Sankeath <s3897982@student.rmit.edu.au>
2022-10-11 17:57:39 +1100

    added account controller functionality

--- Backend/Account/src/main/java/com/example/account/Controller/AccountController.java
+++ Backend/Account/src/main/java/com/example/account/Controller/AccountController.java
@@ -1,21 +1,67 @@
 package com.example.account.Controller;
 
-//import com.example.onlineshop.DAO.OnlineShopDAO;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.MethodNotAllowedException;
-import org.springframework.web.server.ResponseStatusException;
 
+
+import com.example.account.DAO.AccountDAO;
+import com.example.account.Model.Account;
+
+import java.util.List;
+import java.util.Optional;
 
 
 @RestController
-@RequestMapping(path = "/account")
+@RequestMapping(path = "/accounts")
 public class AccountController {
 
+    @Autowired
+    private AccountDAO accountDAO;
 
+    //Get all persons
+    @GetMapping(path = "/account", produces = "application/json")
+    public List<Account> getAccountList(){
+        return accountDAO.getAccountList();
+    }
 
+    //Get person by id
+    @GetMapping(path = "/{id}", produces = "application/json")
+    public Account getAccount(@PathVariable Long id){
+        Optional<Account> person = accountDAO.getAccount(id);
+        return person.get();
+    }
+
+    @PutMapping(path = "/account", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<String> updateAccount(@RequestBody Account account){
+
+        if (accountDAO.updateAccount(account.getId(),account)){
+            return new ResponseEntity<>("Account info updated",HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>("Account to update not found",HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping(path = "/account",consumes = "application/json",produces = "application/json")
+    public ResponseEntity<String> deleteAccount(@RequestBody Account account){
+        accountDAO.deleteAccount(account.getId());
+        return new ResponseEntity<>("Account specified by id was deleted",HttpStatus.OK);
+    }
+
+    @PostMapping(consumes = "application/json",produces = "application/json")
+    public ResponseEntity<String> createAccount(@RequestBody Account account) {
+
+        boolean isCreated;
+
+        isCreated = accountDAO.createAccount(account);
+        if (isCreated) {
+            return new ResponseEntity<>("Account created", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Account not created", HttpStatus.NOT_FOUND);
+        }
+    }
 
 }
